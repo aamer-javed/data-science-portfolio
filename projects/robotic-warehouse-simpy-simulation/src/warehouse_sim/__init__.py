597c0115a71faafb1c4b6fbece742ea4f784fdb3
Aamer Javed <aamer@bu.edu>
2026-07-12 02:12:32 -0400

    Expose advanced simulation helpers in package init

--- projects/robotic-warehouse-simpy-simulation/src/warehouse_sim/__init__.py
+++ projects/robotic-warehouse-simpy-simulation/src/warehouse_sim/__init__.py
@@ -1,14 +1,20 @@
 """Robotic warehouse discrete-event simulation package."""
 
+from warehouse_sim.calibration import apply_calibration, calibrate_from_event_log, compare_to_historical_kpis
 from warehouse_sim.config import WarehouseConfig
+from warehouse_sim.layout import WarehouseLayout
 from warehouse_sim.simulation import SimulationOutputs, WarehouseSimulation, run_scenario, run_scenarios
 
 __all__ = [
     "WarehouseConfig",
+    "WarehouseLayout",
     "SimulationOutputs",
     "WarehouseSimulation",
+    "apply_calibration",
+    "calibrate_from_event_log",
+    "compare_to_historical_kpis",
     "run_scenario",
     "run_scenarios",
 ]
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
