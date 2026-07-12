570e30411282cfa3365dac381a2a09430aaf003a
Aamer Javed <aamer@bu.edu>
2026-07-12 01:53:38 -0400

    Fix package exports

--- projects/robotic-warehouse-simpy-simulation/src/warehouse_sim/__init__.py
+++ projects/robotic-warehouse-simpy-simulation/src/warehouse_sim/__init__.py
@@ -1,14 +1,14 @@
 """Robotic warehouse discrete-event simulation package."""
 
 from warehouse_sim.config import WarehouseConfig
-from warehouse_sim.simulation import SimulationOutputs, WarehouseSimulation, run_replications, run_scenario
+from warehouse_sim.simulation import SimulationOutputs, WarehouseSimulation, run_scenario, run_scenarios
 
 __all__ = [
     "WarehouseConfig",
     "SimulationOutputs",
     "WarehouseSimulation",
-    "run_replications",
     "run_scenario",
+    "run_scenarios",
 ]
 
 __version__ = "0.2.0"
