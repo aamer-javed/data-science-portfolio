302180a7a9daa3822b63f8420acf1b4377ff5007
Aamer Javed <aamer@bu.edu>
2026-07-12 01:52:19 -0400

    Export simulation package API

--- projects/robotic-warehouse-simpy-simulation/src/warehouse_sim/__init__.py
+++ projects/robotic-warehouse-simpy-simulation/src/warehouse_sim/__init__.py
@@ -1,6 +1,14 @@
 """Robotic warehouse discrete-event simulation package."""
 
 from warehouse_sim.config import WarehouseConfig
-from warehouse_sim.simulation import WarehouseSimulation, run_single_scenario
+from warehouse_sim.simulation import SimulationOutputs, WarehouseSimulation, run_replications, run_scenario
 
-__all__ = ["WarehouseConfig", "WarehouseSimulation", "run_single_scenario"]
+__all__ = [
+    "WarehouseConfig",
+    "SimulationOutputs",
+    "WarehouseSimulation",
+    "run_replications",
+    "run_scenario",
+]
+
+__version__ = "0.2.0"
